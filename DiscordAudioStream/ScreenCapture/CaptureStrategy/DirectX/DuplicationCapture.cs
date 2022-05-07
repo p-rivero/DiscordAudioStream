94a083c5dff3a57ce115cee2cca00fe3a4a6d0fb
privero <65060696+p-rivero@users.noreply.github.com>
2022-05-07 20:10:47 +0200

    Add DesktopDuplication monitor capture source

---
+++ DiscordAudioStream/ScreenCapture/CaptureStrategy/DirectX/DuplicationCapture.cs
@@ -0,0 +1,65 @@
+﻿using DiscordAudioStream.ScreenCapture.CaptureStrategy;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using System.Drawing;
+
+namespace DiscordAudioStream
+{
+	// DirectX capture using Windows.Graphics.Capture
+
+	internal class DuplicationCapture : CaptureSource
+	{
+		private static readonly OutputDuplication[] screens;
+		private static readonly SharpDX.Direct3D11.Device d3dDevice;
+
+		// Index of the selected screen to capture
+		private readonly int index;
+
+		static DuplicationCapture()
+		{
+			// Get adapter for GPU 0
+			var adapter = new Factory1().GetAdapter(0);
+			// Get device from adapter
+			d3dDevice = new SharpDX.Direct3D11.Device(adapter);
+
+			// Iterate adapter.Outputs and create OutputDuplication on each
+			screens = new OutputDuplication[adapter.Outputs.Length];
+			for (int i = 0; i < screens.Length; i++)
+			{
+				Output1 o = adapter.Outputs[i].QueryInterface<Output1>();
+				screens[i] = o.DuplicateOutput(d3dDevice);
+			}
+		}
+
+		public DuplicationCapture(int index)
+		{
+			this.index = index;
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			try
+			{
+				screens[index].ReleaseFrame();
+			}
+			catch (SharpDX.SharpDXException)
+			{
+				int i = 5;
+			}
+		}
+
+		public override Bitmap CaptureFrame()
+		{
+			// Try to get duplicated frame
+			screens[index].AcquireNextFrame(-1, out _, out SharpDX.DXGI.Resource screenResource);
+			using (Texture2D texture = screenResource.QueryInterface<Texture2D>())
+			{
+				Bitmap bmp = BitmapHelper.CreateFromTexture2D(texture, d3dDevice);
+				screens[index].ReleaseFrame();
+				screenResource.Dispose();
+				return bmp;
+			}
+		}
+	}
+}
---
+++ DiscordAudioStream/ScreenCapture/CaptureStrategy/DirectX/DuplicationMonitorCapture.cs
@@ -0,0 +1,56 @@
+﻿using SharpDX.DXGI;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DiscordAudioStream.ScreenCapture.CaptureStrategy
+{
+	public class DuplicationMonitorCapture : CaptureSource
+	{
+		private readonly CaptureSource source;
+		private readonly Screen monitor;
+
+		public DuplicationMonitorCapture(Screen monitor, bool captureCursor)
+		{
+			this.monitor = monitor;
+			DuplicationCapture dupCapture = new DuplicationCapture(IndexOf(monitor));
+			if (captureCursor)
+			{
+				CursorPainter paintCursor = new CursorPainter(dupCapture);
+				paintCursor.CaptureAreaRect += GetMonitorArea;
+				source = paintCursor;
+			}
+			else
+			{
+				source = dupCapture;
+			}
+		}
+
+		private int IndexOf(Screen screen)
+		{
+			var adapter = new Factory1().GetAdapter(0);
+			for (int i = 0; i < adapter.Outputs.Length; i++)
+			{
+				if (adapter.Outputs[i].Description.DeviceName == screen.DeviceName)
+					return i;
+			}
+			throw new ArgumentException("Could not find index of screen");
+		}
+
+		public override Bitmap CaptureFrame()
+		{
+			return source.CaptureFrame();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			source.Dispose();
+		}
+
+		private Rectangle GetMonitorArea()
+		{
+			return monitor.Bounds;
+		}
+	}
+}
