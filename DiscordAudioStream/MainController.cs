ca54d5126e7dbe37dcee3cbb2dc632640f44538c
privero <65060696+p-rivero@users.noreply.github.com>
2022-05-14 18:35:47 +0200

    Split MainForm in 2 files
    MainForm is responsible for updating the UI
    MainController is responsible for the actual application logic

---
+++ DiscordAudioStream/MainController.cs
@@ -0,0 +1,357 @@
+﻿using DiscordAudioStream.ScreenCapture;
+using DLLs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DiscordAudioStream
+{
+	internal class MainController
+	{
+		private readonly MainForm form;
+
+		private bool streamEnabled = false;
+		private double scaleMultiplier = 1;
+		private int numberOfScreens = -1;
+
+		private ScreenCaptureManager screenCapture;
+		private ProcessHandleList processHandleList;
+		private readonly CaptureState captureState = new CaptureState();
+
+		private AudioPlayback audioPlayback = null;
+
+
+		public MainController(MainForm owner)
+		{
+			form = owner;
+			processHandleList = ProcessHandleList.Refresh();
+		}
+
+		internal void Init()
+		{
+			RefreshAreaInfo();
+
+			captureState.HideTaskbar = Properties.Settings.Default.HideTaskbar;
+			captureState.CapturingCursor = Properties.Settings.Default.CaptureCursor;
+
+			screenCapture = new ScreenCaptureManager(captureState);
+			screenCapture.CaptureAborted += AbortCapture;
+
+			Thread drawThread = new Thread(() =>
+			{
+				Stopwatch stopwatch = new Stopwatch();
+				const int INTERVAL_MS = 1000 / ScreenCaptureManager.TARGET_FRAMERATE;
+				Size oldSize = new Size(0, 0);
+
+				while (true)
+				{
+					stopwatch.Restart();
+					try
+					{
+						Bitmap next = ScreenCaptureManager.GetNextFrame();
+
+						// Continue iterating until GetNextFrame() doesn't return null
+						if (next == null) continue;
+
+						// Detect size changes
+						if (next.Size != oldSize)
+						{
+							oldSize = next.Size;
+							SetPreviewSize(next.Size);
+						}
+
+						// Display captured frame
+						form.UpdatePreview(next);
+					}
+					catch (InvalidOperationException)
+					{
+						// Form is closing
+						return;
+					}
+					stopwatch.Stop();
+
+					int wait = INTERVAL_MS - (int)stopwatch.ElapsedMilliseconds;
+					if (wait > 0)
+					{
+						Thread.Sleep(wait);
+					}
+				}
+			});
+			drawThread.IsBackground = true;
+			drawThread.Start();
+		}
+
+		// Called when the X button is pressed
+		internal bool Stop()
+		{
+			bool cancel = false;
+			if (streamEnabled)
+			{
+				cancel = true; // Do not close form
+				// Instead, return to settings
+				EndStream();
+			}
+			else
+			{
+				screenCapture?.Stop();
+				User32.UnregisterHotKey(form.Handle, 0);
+			}
+			return cancel;
+		}
+
+
+		// PRIVATE METHODS
+
+		private void SetPreviewSize(Size size)
+		{
+			if (streamEnabled)
+			{
+				size.Width = (int)(Math.Max(160, size.Width) * scaleMultiplier);
+				size.Height = (int)(Math.Max(160, size.Height) * scaleMultiplier);
+				form.SetPreviewUISize(size);
+			}
+		}
+
+		private void RefreshAreaInfo()
+		{
+			if (!form.Created) return;
+
+			int windowIndex = form.VideoIndex - numberOfScreens - 1;
+
+			if (windowIndex == -1)
+			{
+				// Index right before first Window: Custom area
+				captureState.Target = CaptureState.CaptureTarget.CustomArea;
+			}
+			else if (windowIndex >= 0)
+			{
+				// Window
+				captureState.WindowHandle = processHandleList[windowIndex];
+			}
+			else
+			{
+				// Screen
+				if (Screen.AllScreens.Length > 1 && form.VideoIndex == numberOfScreens - 1)
+				{
+					// All screens
+					captureState.Target = CaptureState.CaptureTarget.AllScreens;
+				}
+				else
+				{
+					// Single screen
+					captureState.Screen = Screen.AllScreens[form.VideoIndex];
+				}
+			}
+
+			form.HideTaskbarEnabled = captureState.HideTaskbarSupported;
+			if (captureState.HideTaskbarSupported)
+			{
+				// The selected method allows hiding taskbar, see if checkbox is checked
+				captureState.HideTaskbar = form.HideTaskbar;
+			}
+		}
+
+
+
+		// INTERNAL METHODS (called from MainForm)
+
+		internal void UpdateAreaComboBox(int oldIndex)
+		{
+			IntPtr oldHandle = IntPtr.Zero;
+			if (oldIndex > numberOfScreens)
+			{
+				// We were capturing a window, store its handle
+				int windowIndex = oldIndex - numberOfScreens - 1;
+				oldHandle = processHandleList[windowIndex];
+			}
+
+			// Refresh list of windows
+			RefreshScreens();
+
+			if (oldIndex > numberOfScreens)
+			{
+				// We were capturing a window, see if it still exists
+				int windowIndex = processHandleList.IndexOf(oldHandle);
+				if (windowIndex == -1)
+				{
+					// Window has been closed, return to last saved screen
+					form.VideoIndex = Properties.Settings.Default.AreaIndex;
+				}
+				else
+				{
+					// Window still exists
+					form.VideoIndex = windowIndex + numberOfScreens + 1;
+				}
+			}
+			else
+			{
+				// We were capturing a screen
+				form.VideoIndex = oldIndex;
+			}
+		}
+
+		internal void RefreshScreens()
+		{
+			List<string> elements = new List<string>();
+
+			for (int i = 0; i < Screen.AllScreens.Length; i++)
+			{
+				Rectangle bounds = Screen.AllScreens[i].Bounds;
+				if (Screen.AllScreens[i].Primary)
+				{
+					elements.Add("Primary screen (" + bounds.Width + "x" + bounds.Height + ")");
+				}
+				else
+				{
+					elements.Add("Screen " + (i + 1) + " (" + bounds.Width + "x" + bounds.Height + ")");
+				}
+			}
+			if (Screen.AllScreens.Length > 1)
+			{
+				elements.Add("Everything");
+			}
+
+			numberOfScreens = elements.Count;
+
+			// Item with separator
+			elements.Add(null);
+			elements.Add("Custom area");
+
+			processHandleList = ProcessHandleList.Refresh();
+			foreach (string window in processHandleList.Names)
+			{
+				elements.Add(window);
+			}
+			
+			form.SetVideoItems(elements);
+		}
+
+		internal void RefreshAudioDevices()
+		{
+			List<string> elements = new List<string>();
+
+			elements.Add("(None)");
+
+			foreach (string device in AudioPlayback.RefreshDevices())
+			{
+				elements.Add(device);
+			}
+			
+			form.SetAudioElements(elements);
+		}
+
+
+		internal void StartStream()
+		{
+			if (form.AudioIndex == 0)
+			{
+				DialogResult r = MessageBox.Show("No audio source selected, continue anyways?", "Warning",
+					MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+				if (r == DialogResult.No)
+					return;
+			}
+			else
+			{
+				// Skip "None"
+				audioPlayback = new AudioPlayback(form.AudioIndex - 1);
+				audioPlayback.Start();
+			}
+
+			form.EnableStreamingUI(true);
+			streamEnabled = true;
+
+			// Show preview at full size
+			SetPreviewSize(form.TruePreviewSize);
+		}
+
+		private void EndStream()
+		{
+			form.EnableStreamingUI(false);
+			streamEnabled = false;
+			if (audioPlayback != null)
+			{
+				audioPlayback.Stop();
+			}
+		}
+
+
+		private void AbortCapture()
+		{
+			form.Invoke(new Action(() =>
+			{
+				RefreshScreens();
+				form.VideoIndex = Properties.Settings.Default.AreaIndex;
+				if (!streamEnabled) return;
+
+				EndStream();
+				if (Properties.Settings.Default.AutoExit) form.Close();
+			}));
+		}
+
+		internal void ShowSettingsForm(bool darkMode)
+		{
+			SettingsForm settingsBox = new SettingsForm(darkMode, captureState);
+			settingsBox.Owner = form;
+			settingsBox.CaptureMethodChanged += RefreshAreaInfo;
+
+			// If this window is topmost, settings is too
+			if (form.TopMost) settingsBox.TopMost = true;
+
+			settingsBox.ShowDialog();
+		}
+
+		internal void ShowAboutForm(bool darkMode)
+		{
+			AboutForm aboutBox = new AboutForm(darkMode);
+			aboutBox.Owner = form;
+			if (form.TopMost)
+			{
+				aboutBox.TopMost = true;
+			}
+			aboutBox.ShowDialog();
+		}
+
+		internal void SetVideoIndex(int index)
+		{
+			if (numberOfScreens == -1) return;
+
+			RefreshAreaInfo();
+
+			// Do not save settings for Custom Area or Window
+			if (index < numberOfScreens)
+			{
+				Properties.Settings.Default.AreaIndex = index;
+				Properties.Settings.Default.Save();
+			}
+		}
+
+		internal void SetScaleIndex(int index)
+		{
+			if (index == 0) scaleMultiplier = 1;              // Original resolution
+			else if (index == 1) scaleMultiplier = Math.Sqrt(0.50); // 50% resolution
+			else if (index == 2) scaleMultiplier = Math.Sqrt(0.25); // 25% resolution
+			else throw new ArgumentException("Invalid index");
+
+			Properties.Settings.Default.ScaleIndex = index;
+			Properties.Settings.Default.Save();
+		}
+
+		internal void SetHideTaskbar(bool hideTaskbar)
+		{
+			captureState.HideTaskbar = hideTaskbar;
+			Properties.Settings.Default.HideTaskbar = hideTaskbar;
+			Properties.Settings.Default.Save();
+		}
+
+		internal void SetCapturingCursor(bool capturing)
+		{
+			captureState.CapturingCursor = capturing;
+			Properties.Settings.Default.CaptureCursor = capturing;
+			Properties.Settings.Default.Save();
+		}
+	}
+}
--- DiscordAudioStream/MainForm.cs
+++ DiscordAudioStream/MainForm.cs
@@ -2,40 +2,32 @@
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
-using System.Threading;
 using System.Diagnostics;
 using CustomComponents;
-using DiscordAudioStream.ScreenCapture;
 using DLLs;
+using System.Collections.Generic;
 
 namespace DiscordAudioStream
 {
 	public partial class MainForm : Form
 	{
+		// Encapsulates all business logic in a controller and let the form handle only UI
+		private readonly MainController controller;
+
 		private readonly bool darkMode;
 		private readonly Size defaultWindowSize;
 		private readonly Size defaultPreviewSize;
 		private readonly Point defaultPreviewLocation;
-		private bool streamEnabled = false;
-
-		private double scaleMultiplier = 1;
-		private int numberOfScreens = -1;
-
-		private ScreenCaptureManager screenCapture;
-		private ProcessHandleList processHandleList;
-		private readonly CaptureState captureState = new CaptureState();
-
-		private AudioPlayback audioPlayback = null;
 		
 		public MainForm(bool darkMode)
 		{
+			controller = new MainController(this);
+
+			this.darkMode = darkMode;
 			if (darkMode)
 			{
 				this.HandleCreated += new EventHandler(DarkThemeManager.FormHandleCreated);
 			}
-
-			this.darkMode = darkMode;
-			processHandleList = ProcessHandleList.Refresh();
 
 			InitializeComponent();
 			previewBox.Visible = true;
@@ -46,76 +38,141 @@ namespace DiscordAudioStream
 
 			inputDeviceComboBox.SelectedIndex = 0;
 
-			RefreshScreens();
-			RefreshAudioDevices();
+			controller.RefreshScreens();
+			controller.RefreshAudioDevices();
 			try
 			{
-				areaComboBox.SelectedIndex = Properties.Settings.Default.AreaIndex;
+				VideoIndex = Properties.Settings.Default.AreaIndex;
 			}
 			catch (ArgumentOutOfRangeException)
 			{
 				// Number of screen may have changed
-				areaComboBox.SelectedIndex = 0;
+				VideoIndex = 0;
 			}
 
 			previewBtn.Checked = Properties.Settings.Default.Preview;
-			EnablePreview(previewBtn.Checked);
+			DisplayPreview(previewBtn.Checked);
 
-			areaComboBox.SelectedIndex = Properties.Settings.Default.AreaIndex;
+			VideoIndex = Properties.Settings.Default.AreaIndex;
 			scaleComboBox.SelectedIndex = Properties.Settings.Default.ScaleIndex;
 
 			ApplyDarkTheme(darkMode);
 		}
 
+		// INTERNAL METHODS (called from controller)
 
-		// PUBLIC METHODS
-
-		public void SetPreviewSize(Size size)
+		internal int VideoIndex
 		{
-			if (streamEnabled)
-			{
-				size.Width = (int)(Math.Max(160, size.Width) * scaleMultiplier);
-				size.Height = (int)(Math.Max(160, size.Height) * scaleMultiplier);
-				BeginInvoke(new Action(() => previewBox.Size = size));
-			}
+			get { return areaComboBox.SelectedIndex; }
+			set { areaComboBox.SelectedIndex = value; }
 		}
 
-		public void UpdateAreaComboBox()
+		internal void SetVideoItems(IEnumerable<string> items)
 		{
-			int oldIndex = areaComboBox.SelectedIndex;
-			IntPtr oldHandle = IntPtr.Zero;
-			if (oldIndex > numberOfScreens)
+			areaComboBox.Items.Clear();
+			bool addSeparator = false;
+			int numSeparators = 0;
+			foreach (string item in items)
 			{
-				// We were capturing a window, store its handle
-				int windowIndex = oldIndex - numberOfScreens - 1;
-				oldHandle = processHandleList[windowIndex];
-			}
-
-			// Refresh list of windows
-			RefreshScreens();
-
-			if (oldIndex > numberOfScreens)
-			{
-				// We were capturing a window, see if it still exists
-				int windowIndex = processHandleList.IndexOf(oldHandle);
-				if (windowIndex == -1)
+				// Use null to indicate that the next element has a separator
+				if (item == null)
 				{
-					// Window has been closed, return to last saved screen
-					areaComboBox.SelectedIndex = Properties.Settings.Default.AreaIndex;
+					addSeparator = true;
+				}
+				else if (addSeparator)
+				{
+					areaComboBox.Items.Add(new DarkThemeComboBox.ItemWithSeparator("Custom area"));
+					numSeparators++;
+					addSeparator = false;
 				}
 				else
 				{
-					// Window still exists
-					areaComboBox.SelectedIndex = windowIndex + numberOfScreens + 1;
+					areaComboBox.Items.Add(item);
 				}
 			}
-			else
+			// For each separator, we need to add a dummy element at the end
+			for (int i = 0; i < numSeparators; i++)
 			{
-				// We were capturing a screen
-				areaComboBox.SelectedIndex = oldIndex;
+				areaComboBox.Items.Add(new DarkThemeComboBox.Dummy());
 			}
 		}
 
+		internal int AudioIndex
+		{
+			get { return inputDeviceComboBox.SelectedIndex; }
+		}
+
+		internal void SetAudioElements(IEnumerable<string> elements)
+		{
+			inputDeviceComboBox.Items.Clear();
+
+			foreach (string element in elements)
+			{
+				inputDeviceComboBox.Items.Add(element);
+			}
+
+			inputDeviceComboBox.SelectedIndex = 0;
+		}
+
+		internal bool HideTaskbar
+		{
+			get { return hideTaskbarCheckBox.Checked; }
+		}
+		internal bool HideTaskbarEnabled
+		{
+			get { return hideTaskbarCheckBox.Enabled; }
+			set
+			{
+				hideTaskbarCheckBox.Enabled = value;
+				// When disabling the checkbox, also set its state to unchecked
+				if (!value) hideTaskbarCheckBox.Checked = false;
+			}
+		}
+
+
+		internal void SetPreviewUISize(Size newSize)
+		{
+			BeginInvoke(new Action(() => previewBox.Size = newSize));
+		}
+		internal Size TruePreviewSize
+		{
+			get { return previewBox.Image.Size; }
+		}
+
+		internal void EnableStreamingUI(bool streaming)
+		{
+			// Autosize the form only while streaming
+			this.AutoSize = streaming;
+
+			// Disable UI elements while streaming
+			videoGroup.Visible = !streaming;
+			audioGroup.Visible = !streaming;
+			startButton.Visible = !streaming;
+			toolStrip.Visible = !streaming;
+
+			if (streaming)
+			{
+				// If we start streaming, override previewBtn and enable the previewBox
+				DisplayPreview(true);
+				previewBox.Location = new Point(0, 0);
+			}
+			else
+			{
+				DisplayPreview(previewBtn.Checked);
+				previewBox.Size = defaultPreviewSize;
+				previewBox.Location = defaultPreviewLocation;
+				CenterToScreen();
+			}
+		}
+
+		internal void UpdatePreview(Bitmap newImage)
+		{
+			Invoke(new Action(() =>
+			{
+				previewBox.Image?.Dispose();
+				previewBox.Image = newImage;
+			}));
+		}
 
 
 		// PRIVATE METHODS
@@ -148,149 +205,7 @@ namespace DiscordAudioStream
 			hideTaskbarCheckBox.SetDarkMode(darkMode);
 		}
 
-		private void RefreshAreaInfo()
-		{
-			if (!Created) return;
-
-			int windowIndex = areaComboBox.SelectedIndex - numberOfScreens - 1;
-
-			if (windowIndex == -1)
-			{
-				// Index right before first Window: Custom area
-				captureState.Target = CaptureState.CaptureTarget.CustomArea;
-			}
-			else if (windowIndex >= 0)
-			{
-				// Window
-				captureState.WindowHandle = processHandleList[windowIndex];
-			}
-			else
-			{
-				// Screen
-				if (Screen.AllScreens.Length > 1 && areaComboBox.SelectedIndex == numberOfScreens - 1)
-				{
-					// All screens
-					captureState.Target = CaptureState.CaptureTarget.AllScreens;
-				}
-				else
-				{
-					// Single screen
-					captureState.Screen = Screen.AllScreens[areaComboBox.SelectedIndex];
-				}
-			}
-
-			hideTaskbarCheckBox.Enabled = captureState.HideTaskbarSupported;
-			if (captureState.HideTaskbarSupported)
-			{
-				captureState.HideTaskbar = hideTaskbarCheckBox.Checked;
-			}
-		}
-
-		private void RefreshAudioDevices()
-		{
-			inputDeviceComboBox.Items.Clear();
-			inputDeviceComboBox.Items.Add("(None)");
-
-			foreach (string device in AudioPlayback.RefreshDevices())
-			{
-				inputDeviceComboBox.Items.Add(device);
-			}
-			inputDeviceComboBox.SelectedIndex = 0;
-		}
-
-		private void RefreshScreens()
-		{
-			areaComboBox.Items.Clear();
-
-			for (int i = 0; i < Screen.AllScreens.Length; i++)
-			{
-				Rectangle bounds = Screen.AllScreens[i].Bounds;
-				if (Screen.AllScreens[i].Primary)
-				{
-					areaComboBox.Items.Add("Primary screen (" + bounds.Width + "x" + bounds.Height + ")");
-				}
-				else
-				{
-					areaComboBox.Items.Add("Screen " + (i + 1) + " (" + bounds.Width + "x" + bounds.Height + ")");
-				}
-			}
-			if (Screen.AllScreens.Length > 1)
-			{
-				areaComboBox.Items.Add("Everything");
-			}
-
-			numberOfScreens = areaComboBox.Items.Count;
-
-			areaComboBox.Items.Add(new DarkThemeComboBox.ItemWithSeparator("Custom area"));
-
-			processHandleList = ProcessHandleList.Refresh();
-			foreach (string window in processHandleList.Names)
-			{
-				areaComboBox.Items.Add(window);
-			}
-			areaComboBox.Items.Add(new DarkThemeComboBox.Dummy());
-		}
-
-		private void StartStream()
-		{
-			if (inputDeviceComboBox.SelectedIndex == 0)
-			{
-				DialogResult r = MessageBox.Show("No audio source selected, continue anyways?", "Warning",
-					MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-
-				if (r == DialogResult.No)
-					return;
-			}
-			else
-			{
-				// Skip "None"
-				audioPlayback = new AudioPlayback(inputDeviceComboBox.SelectedIndex - 1);
-				audioPlayback.Start();
-			}
-
-			EnablePreview(true);
-			previewBox.Location = new Point(0, 0);
-			videoGroup.Visible = false;
-			audioGroup.Visible = false;
-			startButton.Visible = false;
-			toolStrip.Visible = false;
-			streamEnabled = true;
-			this.AutoSize = true;
-
-			SetPreviewSize(previewBox.Image.Size);
-		}
-
-		private void EndStream()
-		{
-			videoGroup.Visible = true;
-			audioGroup.Visible = true;
-			startButton.Visible = true;
-			toolStrip.Visible = true;
-			streamEnabled = false;
-
-			previewBox.Size = defaultPreviewSize;
-			previewBox.Location = defaultPreviewLocation;
-			this.AutoSize = false;
-			if (audioPlayback != null) audioPlayback.Stop();
-
-			EnablePreview(previewBtn.Checked);
-			CenterToScreen();
-		}
-
-		private void AbortCapture()
-		{
-			Invoke(new Action(() =>
-			{
-				RefreshScreens();
-				areaComboBox.SelectedIndex = Properties.Settings.Default.AreaIndex;
-				if (!streamEnabled) return;
-
-				EndStream();
-				if (Properties.Settings.Default.AutoExit) Close();
-			}));
-		}
-
-		private void EnablePreview(bool visible)
+		private void DisplayPreview(bool visible)
 		{
 			previewBox.Visible = visible;
 
@@ -314,68 +229,14 @@ namespace DiscordAudioStream
 
 
 
-
 		// EVENTS
 
 		private void MainForm_Load(object sender, EventArgs e)
 		{
+			controller.Init();
 			onTopBtn.Checked = Properties.Settings.Default.AlwaysOnTop;
 			captureCursorCheckBox.Checked = Properties.Settings.Default.CaptureCursor;
 			hideTaskbarCheckBox.Checked = Properties.Settings.Default.HideTaskbar;
-			RefreshAreaInfo();
-
-			captureState.HideTaskbar = hideTaskbarCheckBox.Checked;
-			captureState.CapturingCursor = captureCursorCheckBox.Checked;
-
-			screenCapture = new ScreenCaptureManager(captureState);
-			screenCapture.CaptureAborted += AbortCapture;
-
-			Thread drawThread = new Thread(() =>
-			{
-				Stopwatch stopwatch = new Stopwatch();
-				const int INTERVAL_MS = 1000 / ScreenCaptureManager.TARGET_FRAMERATE;
-				Size oldSize = new Size(0, 0);
-
-				while (true)
-				{
-					stopwatch.Restart();
-					try
-					{
-						Bitmap next = ScreenCaptureManager.GetNextFrame();
-
-						// Continue iterating until GetNextFrame() doesn't return null
-						if (next == null) continue;
-
-						// Detect size changes
-						if (next.Size != oldSize)
-						{
-							oldSize = next.Size;
-							SetPreviewSize(next.Size);
-						}
-
-						// Display captured frame
-						Invoke(new Action(() =>
-						{
-							previewBox.Image?.Dispose();
-							previewBox.Image = next;
-						}));
-					}
-					catch (InvalidOperationException)
-					{
-						// Form is closing
-						return;
-					}
-					stopwatch.Stop();
-
-					int wait = INTERVAL_MS - (int)stopwatch.ElapsedMilliseconds;
-					if (wait > 0)
-					{
-						Thread.Sleep(wait);
-					}
-				}
-			});
-			drawThread.IsBackground = true;
-			drawThread.Start();
 		}
 
 		protected override void OnFormClosing(FormClosingEventArgs e)
@@ -384,18 +245,8 @@ namespace DiscordAudioStream
 
 			if (e.CloseReason == CloseReason.WindowsShutDown) return;
 
-			if (streamEnabled)
-			{
-				e.Cancel = true; // Do not close form
-
-				// Instead, return to settings
-				EndStream();
-			}
-			else
-			{
-				screenCapture?.Stop();
-				User32.UnregisterHotKey(this.Handle, 0);
-			}
+			// MainController.Stop() returns false if the form has to be closed
+			e.Cancel = controller.Stop();
 		}
 
 		private void MainForm_KeyDown(object sender, KeyEventArgs e)
@@ -438,7 +289,7 @@ namespace DiscordAudioStream
 			Properties.Settings.Default.Preview = previewBtn.Checked;
 			Properties.Settings.Default.Save();
 
-			EnablePreview(previewBtn.Checked);
+			DisplayPreview(previewBtn.Checked);
 		}
 
 		private void onTopCheckBox_CheckedChanged(object sender, EventArgs e)
@@ -485,89 +336,53 @@ namespace DiscordAudioStream
 
 		private void settingsBtn_Click(object sender, EventArgs e)
 		{
-			SettingsForm settingsBox = new SettingsForm(darkMode, captureState);
-			settingsBox.Owner = this;
-			settingsBox.CaptureMethodChanged += RefreshAreaInfo;
-
-			// If this window is topmost, settings is too
-			if (TopMost) settingsBox.TopMost = true;
-			
-			settingsBox.ShowDialog();
+			controller.ShowSettingsForm(darkMode);
 		}
 
 		private void aboutBtn_Click(object sender, EventArgs e)
 		{
-			AboutForm aboutBox = new AboutForm(darkMode);
-			aboutBox.Owner = this;
-			if (this.TopMost)
-			{
-				aboutBox.TopMost = true;
-			}
-			aboutBox.ShowDialog();
+			controller.ShowAboutForm(darkMode);
 		}
 
 		private void startButton_Click(object sender, EventArgs e)
 		{
-			StartStream();
+			controller.StartStream();
 		}
-
-
 
 		private void areaComboBox_DropDown(object sender, EventArgs e)
 		{
-			UpdateAreaComboBox();
+			// When the user expands the area combobox, update its elements
+			controller.UpdateAreaComboBox(VideoIndex);
 		}
 
 		private void areaComboBox_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			if (numberOfScreens == -1)
-				return;
-
 			// Do not select the dummy object at the end of the list
-			if (areaComboBox.SelectedIndex == areaComboBox.Items.Count - 1)
+			if (VideoIndex == areaComboBox.Items.Count - 1)
 			{
-				areaComboBox.SelectedIndex = areaComboBox.Items.Count - 2;
+				VideoIndex = areaComboBox.Items.Count - 2;
 			}
-
-			RefreshAreaInfo();
-			// Do not save settings for Custom Area or Window
-			if (areaComboBox.SelectedIndex >= numberOfScreens)
-				return;
-
-			Properties.Settings.Default.AreaIndex = areaComboBox.SelectedIndex;
-			Properties.Settings.Default.Save();
+			controller.SetVideoIndex(VideoIndex);
 		}
 
 		private void scaleComboBox_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			int index = scaleComboBox.SelectedIndex;
-			if (index == 0) scaleMultiplier = 1;              // Original resolution
-			else if (index == 1) scaleMultiplier = Math.Sqrt(0.50); // 50% resolution
-			else if (index == 2) scaleMultiplier = Math.Sqrt(0.25); // 25% resolution
-			else throw new ArgumentException("Invalid index");
-
-			Properties.Settings.Default.ScaleIndex = index;
-			Properties.Settings.Default.Save();
+			controller.SetScaleIndex(scaleComboBox.SelectedIndex);
 		}
 
 		private void hideTaskbarCheckBox_CheckedChanged(object sender, EventArgs e)
 		{
-			RefreshAreaInfo();
-			Properties.Settings.Default.HideTaskbar = hideTaskbarCheckBox.Checked;
-			Properties.Settings.Default.Save();
+			controller.SetHideTaskbar(hideTaskbarCheckBox.Checked);
 		}
 
 		private void captureCursorCheckBox_CheckedChanged(object sender, EventArgs e)
 		{
-			captureState.CapturingCursor = captureCursorCheckBox.Checked;
-
-			Properties.Settings.Default.CaptureCursor = captureCursorCheckBox.Checked;
-			Properties.Settings.Default.Save();
+			controller.SetCapturingCursor(captureCursorCheckBox.Checked);
 		}
 
 		private void refreshBtn_Click(object sender, EventArgs e)
 		{
-			RefreshAudioDevices();
+			controller.RefreshAudioDevices();
 		}
 	}
 }
