ed6f1b93a169191cc3f8f227e2aa38bde6eda6c6
privero <65060696+p-rivero@users.noreply.github.com>
2022-06-13 08:53:01 +0200

    Do not capture cursor by default

--- DiscordAudioStream/Properties/Settings.Designer.cs
+++ DiscordAudioStream/Properties/Settings.Designer.cs
@@ -49,7 +49,7 @@ namespace DiscordAudioStream.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool CaptureCursor {
             get {
                 return ((bool)(this["CaptureCursor"]));
