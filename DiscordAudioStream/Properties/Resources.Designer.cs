0de76031380729c7ad7a025f9da524a2b703bc54
privero <65060696+p-rivero@users.noreply.github.com>
2023-06-30 17:04:44 +0200

    Update resources

--- DiscordAudioStream/Properties/Resources.Designer.cs
+++ DiscordAudioStream/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace DiscordAudioStream.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Mono_Options {
+            get {
+                object obj = ResourceManager.GetObject("Mono_Options", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap move {
