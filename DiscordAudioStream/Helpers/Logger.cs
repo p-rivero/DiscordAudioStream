0ebdfdc081ae56b2257a232110e32c2373c084b9
privero <65060696+p-rivero@users.noreply.github.com>
2023-09-10 16:59:35 +0200

    Update logger

--- DiscordAudioStream/Helpers/Logger.cs
+++ DiscordAudioStream/Helpers/Logger.cs
@@ -7,8 +7,13 @@ namespace DiscordAudioStream
 	internal static class Logger
 	{
 		const string LOG_FILE_PATH = "DiscordAudioStream_log.txt";
+		
+		const int GROUP_TIME_DELTA_MS = 50;
 
-		private static readonly int START_TIME = Environment.TickCount;
+		private static readonly int startTime = Environment.TickCount;
+
+		private static string groupCallerName = null;
+		private static int groupLastLogTime = 0;
 
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("SonarQube", "S3963", Justification = 
 			"Cannot convert this static constructor into an inline initialization")]
@@ -32,37 +37,60 @@ namespace DiscordAudioStream
 		
 		public static void EmptyLine()
 		{
+			ForceStartNewGroup();
 			Trace.WriteLine("");
 		}
 		
 		public static void Log(string text)
 		{
-			LogImpl(text);
+			int timestamp = GetTimestamp();
+			string callerName = GetCallerName();
+			if (!GroupWithPreviousLogs(timestamp, callerName))
+			{
+				Trace.WriteLine($"\n{timestamp}ms [{callerName}]");
+			}
+			Trace.WriteLine("    " + text);
+			groupCallerName = callerName;
+			groupLastLogTime = timestamp;
 		}
 
 		public static void Log(Exception e)
 		{
 			string separator = new string('-', 40);
-			LogImpl($"Exception:\n{separator}\n{e}\n{separator}");
+			Log($"Exception:\n{separator}\n{e}\n{separator}");
 		}
 
-
-		private static void LogImpl(string text)
-		{
-			Trace.WriteLine($"{GetTimestamp()}ms [{GetCallerName(3)}]\n    {text}");
-		}
-
-		private static string GetCallerName(int stackDepth)
+		private static string GetCallerName()
 		{
 			StackTrace stackTrace = new StackTrace();
-			StackFrame frame = stackTrace.GetFrame(stackDepth);
-			System.Reflection.MethodBase method = frame.GetMethod();
-			return $"{method.DeclaringType.Name}.{method.Name}";
+			// Go down the stack until a method that isn't in this class is found
+			for (int i = 0; i < stackTrace.FrameCount; i++)
+			{
+				StackFrame frame = stackTrace.GetFrame(i);
+				System.Reflection.MethodBase method = frame.GetMethod();
+				if (method.DeclaringType != typeof(Logger))
+				{
+					return $"{method.DeclaringType.Name}.{method.Name}";
+				}
+			}
+			return "Unknown";
 		}
 
 		private static int GetTimestamp()
 		{
-			return Environment.TickCount - START_TIME;
+			return Environment.TickCount - startTime;
+		}
+
+		private static bool GroupWithPreviousLogs(int timestamp, string callerName)
+		{
+			bool longTimeSinceLastLog = timestamp - groupLastLogTime > GROUP_TIME_DELTA_MS;
+			bool sameCaller = callerName == groupCallerName;
+			return !longTimeSinceLastLog && sameCaller;
+		}
+
+		private static void ForceStartNewGroup()
+		{
+			groupCallerName = null;
 		}
 	}
 }
