59cb19a718656adc7fb8fb4a8ed7981f2a58062f
privero <65060696+p-rivero@users.noreply.github.com>
2023-09-11 14:13:46 +0200

    Fix window capture not working on Windows 7

--- DiscordAudioStream/ScreenCapture/CaptureStrategy/WindowCapture.cs
+++ DiscordAudioStream/ScreenCapture/CaptureStrategy/WindowCapture.cs
@@ -10,19 +10,40 @@ namespace DiscordAudioStream.ScreenCapture.CaptureStrategy
 		{
 			// Get size of client area (don't use X and Y, these are relative to the WINDOW rect)
 			bool success = User32.GetClientRect(windowHandle, out User32.Rect clientRect);
-			if (!success)
+			CheckSuccess(success);
+
+			User32.Rect frame;
+			try
 			{
-				throw new InvalidOperationException("Window was closed");
+				// Get frame size and position (generally more accurate than GetWindowRect)
+				frame = Dwmapi.GetRectAttr(windowHandle, Dwmapi.DwmWindowAttribute.EXTENDED_FRAME_BOUNDS);
 			}
-
-			// Get frame size and position (generally more accurate than GetWindowRect)
-			User32.Rect frame = Dwmapi.GetRectAttr(windowHandle, Dwmapi.DwmWindowAttribute.EXTENDED_FRAME_BOUNDS);
-
+			catch (InvalidOperationException)
+			{
+				return GetWindowAreaFallback(windowHandle);
+			}
+			
 			// Trim the black bar at the top when the window is maximized,
 			// as well as the title bar for applications with a defined client area
 			int yOffset = frame.Height - clientRect.Height;
 
 			return new Rectangle(frame.left + 1, frame.top + yOffset, clientRect.Width, clientRect.Height);
+		}
+
+		private static Rectangle GetWindowAreaFallback(IntPtr windowHandle)
+		{
+			// GetWindowRect is not always accurate when the window is maximized, but doesn't fail on Windows 7
+			bool success = User32.GetWindowRect(windowHandle, out User32.Rect windowRect);
+			CheckSuccess(success);
+			return new Rectangle(windowRect.left, windowRect.top, windowRect.Width, windowRect.Height);
+		}
+
+		private static void CheckSuccess(bool success)
+		{
+			if (!success)
+			{
+				throw new InvalidOperationException("Window was closed");
+			}
 		}
 	}
 }
